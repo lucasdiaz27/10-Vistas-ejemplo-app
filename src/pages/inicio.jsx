a0aea8f2869897a2d5a562a42f5f7731b0b8e5f1
Lucas Diaz <lucasdiaz9087@gmail.com>
2025-04-09 16:22:56 -0300

    primer commit

---
+++ src/pages/inicio.jsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
+function inicio() {
+  const navigate = useNavigate();
+
+  const irAlFormulario = () => {
+    navigate("/formulario"); // Redirige al componente Formulario
+  };
+
+  return (
+    <div className="container text-center mt-5">
+      {/* Tarjeta con Bootstrap */}
+      <div className="card p-5 shadow-lg">
+        <h1 className="mb-3">Bienvenido a SITE</h1>
+        <p className="mb-4">Sistema Interno de Traspasos y Expedientes</p>
+
+        {/* Boton para ir al formulario */}
+        <button onClick={irAlFormulario} className="btn btn-primary">
+          Ir al Formulario
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default inicio;
