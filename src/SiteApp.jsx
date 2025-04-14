1300f1839311d8025db6da66c3b519a75596ac88
alisho <alejorea2015@gmail.com>
2025-04-14 11:28:04 -0300

    Agregué la Navbar y la página de Consultas. También creé 2 componentes nuevos.

--- src/SiteApp.jsx
+++ src/SiteApp.jsx
@@ -1,16 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {Formulario} from './pages/Formulario'
+import NavBar from './components/NavBar';
+import { Formulario } from './pages/Formulario'
 import { Inicio } from "./pages/inicio";
 import { FormPersona } from "./components/FormPersona";
+import Consulta from "./pages/consulta";
 
 function SiteApp() {
   return (
     <Router>
+      <NavBar />
+
       <Routes>
         <Route path="/" element={<Inicio />} />
-        <Route path="/formulario" element={<Formulario/>} />
-        <Route path="/formularioPersona" element={<FormPersona/>} />
+        <Route path="/formulario" element={<Formulario />} />
+        <Route path="/formularioPersona" element={<FormPersona />} />
+        <Route path="/consulta" element={<Consulta />} />
       </Routes>
+
     </Router>
   );
 }
