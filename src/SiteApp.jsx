09ccd0b0739dbdf958403b6912ef0cf90ddb879e
Leonel <leonelbruno972@gmail.com>
2025-04-18 19:30:10 -0300

    Hice el Footer y  actualice un poco el SiteApp para que la navbar y el Footer se queden en su lugares

--- src/SiteApp.jsx
+++ src/SiteApp.jsx
@@ -1,25 +1,29 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from './components/NavBar';
+import Footer from "./components/Footer/Footer";
+import { Formulario } from './pages/Formulario'
+import { Inicio } from "./pages/inicio";
 import { FormPersona } from "./components/FormPersona";
 import Consulta from "./pages/consulta";
-import { Formulario } from "./pages/formulario";
-import Inicio from "./pages/inicio";
 import Login from "./pages/Login";
 
 function SiteApp() {
   return (
     <Router>
+      <div className="d-flex flex-column min-vh-100">
       <NavBar />
-
+      <main className="flex-grow-1">
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/formulario" element={<Formulario />} />
-        <Route path="/formularioPersona" element={<FormPersona />} />
+        <Route path="/" element={<Inicio/>} />
+        <Route path="/formulario" element={<Formulario/>} />
+        <Route path="/formularioPersona" element={<FormPersona/>}/>
         <Route path="/consulta" element={<Consulta />} />
         <Route path="/login" element={<Login />} />
 
       </Routes>
-
+      </main>
+      <Footer/>
+      </div>
     </Router>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+export const Footer = () => {
+    return (
+        
+        
+            <footer className="text-white py-4 bg-dark">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-md-3 d-flex justify-content-center justify-content-md-start mb-3 mb-md-0">
+                        <Link to = "/" className="navbar-brand">
+                        <img src="Logo.png" alt="Logo" width="120" />
+                        </Link>
+                        </div>
+                        
+                        <div className="col-md-9  text-md-end">
+                        <ul className="list-unstyled mb-0">
+                            <li className="font-weight-bold mb-1">Directora: Dra. Luciana Montinho</li>
+                            <li className="font-weight-bold mb-1">25 de Mayo 35 CP (4200)</li>
+                            <li className="font-weight-bold mb-1">(0385) 4211062 / 4218461 / 0800 888 3030</li>
+                            <li className="font-weight-bold mb-1">Email: areacomerciosde@hotmail.com</li>
+                        </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        
+        
+    )
+};
+
+export default Footer;
