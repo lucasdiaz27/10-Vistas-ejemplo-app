3ea01cbdcf2233ec21ad40c194a17accd71d7b23
Leonel <leonelbruno972@gmail.com>
2025-07-12 12:29:29 -0300

    Arregle un error de la Ruta en el import de usuario api

--- src/pages/Ajustes.jsx
+++ src/pages/Ajustes.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Swal from "sweetalert2";
-import { obtenerPerfilUsuario, actualizarNombre, cambiarPassword } from "../api/usuarioAPI";
+import { obtenerPerfilUsuario, actualizarNombre, cambiarPassword } from "../apis/usuarioAPI";
 
 
 const Ajustes = () => {
