3a05e04d68b48ac351196aba29875c3c1c0e0b5f
Lucas Diaz <lucasdiaz9087@gmail.com>
2025-07-16 22:16:12 -0300

    otro cambio 2.0

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import "./SideBar.css";
 import { useState } from "react";
@@ -9,6 +9,7 @@ export const SideBar = ({ abierto, setAbierto }) => {
     setAbierto(!abierto);
   };
 
+  const navigate = useNavigate();
   const handleLogout = () => {
     localStorage.removeItem("token");
     navigate("/login");
