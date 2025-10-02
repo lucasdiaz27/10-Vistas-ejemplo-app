cd94bfcca4ef3ecb9cd4dea20e38d33675233920
alisho0 <alejorea2015@gmail.com>
2025-10-01 21:28:12 -0300

    Cambio para ver usuarios en el rol de Dirección

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -114,7 +114,7 @@ export const SideBar = ({ abierto, setAbierto }) => {
                 </Link>
               </li>
               {rol ==
-                "ADMIN" && (
+                "DIRECCION" && (
                   <li>
                     <Link
                       to="/menu-interno?vista=usuarios"
